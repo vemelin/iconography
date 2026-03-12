540ca6b5de1ee9cd28742ad4cae3e1783ff636cc
Vasiliy Emelin <vemelin@gmail.com>
2026-03-12 16:08:29 -0500

    Fix: upload js change access from public to private

--- api/upload.js
+++ api/upload.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
     const { filename, buffer } = formData.file;
 
     const blob = await put(filename, buffer, {
-      access: 'public',
+      access: 'private',
     });
 
     return res.status(200).json({
